207d06bb03e449f79d63fada2a95abdb0844d22b
nawawan <mizutake0428@yahoo.co.jp>
2021-03-18 18:44:33 +0900

    付属のゴミ

---
+++ src/stage/tilesets.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="tilesets" tilewidth="16" tileheight="16" tilecount="528" columns="8">
+ <image source="tilesets.png" width="128" height="1056"/>
+</tileset>
