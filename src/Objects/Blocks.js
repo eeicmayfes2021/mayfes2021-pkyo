e70d660917ebfd779f8441aee5b8f4d353d02a11
NaomiatLibrary <ray.oshikawa@gmail.com>
2021-03-23 19:35:05 +0900

    ブロックを色々追加

--- src/Objects/Blocks.js
+++ src/Objects/Blocks.js
@@ -17,6 +17,38 @@ Blockly.Blocks['move'] = {
     var code = `this.tryMove(this.player,${dropdown_direction});yield "${block.id}";\n`;
     return code;
   };
+  Blockly.Blocks['moveforward'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("前に動く")
+      this.setNextStatement(true);
+      this.setPreviousStatement(true);
+      this.setColour(270);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+};
+  Blockly.JavaScript['moveforward'] = function(block) {
+    var code = `this.tryMove(this.player,this.getDirection(this.player));yield "${block.id}";\n`;
+    return code;
+  };
+  Blockly.Blocks['turn'] = {
+    init: function() {
+      this.appendDummyInput()
+          .appendField("次の方向に曲がる:")
+          .appendField(new Blockly.FieldDropdown([["右", "0"],["左", "1"]]), "turn_direction");
+      this.setNextStatement(true);
+      this.setPreviousStatement(true);
+      this.setColour(270);
+      this.setTooltip("");
+      this.setHelpUrl("");
+    }
+};
+  Blockly.JavaScript['turn'] = function(block) {
+    var dropdown_direction = block.getFieldValue('turn_direction');
+    var code = `this.changeDirection(this.player,${dropdown_direction});yield "${block.id}";\n`;
+    return code;
+  };
   Blockly.Blocks['while'] = {
     init: function() {
       this.appendDummyInput()
@@ -49,3 +81,47 @@ Blockly.Blocks['move'] = {
     var code = `this.removeObstacle(this.player);yield "${block.id}";\n`;
     return code;
   };
+
+Blockly.Blocks['if'] = {
+  init: function() {
+    this.appendDummyInput().appendField("もし");
+    this.appendValueInput("condition")
+        .setCheck(null);
+    this.appendDummyInput().appendField("なら");
+    this.appendStatementInput("iftrue");
+    this.appendDummyInput().appendField("そうでないなら");
+    this.appendStatementInput("iffalse");
+    this.setNextStatement(true);
+    this.setPreviousStatement(true);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['if'] = function(block) {
+  var value_condition = Blockly.JavaScript.valueToCode(block, 'condition', Blockly.JavaScript.ORDER_ATOMIC);
+  var statements_iftrue = Blockly.JavaScript.statementToCode(block, 'iftrue');
+  var statements_iffalse = Blockly.JavaScript.statementToCode(block, 'iffalse');
+  // TODO: Assemble JavaScript into code variable.
+  var code = `if(${value_condition}){${statements_iftrue}}\nelse{${statements_iffalse}};\n`;
+  return code;
+};
+Blockly.Blocks['check'] = {
+  init: function() {
+    this.appendDummyInput()
+        .appendField(new Blockly.FieldDropdown([["前","0"], ["右","1"], ["左","2"],["後ろ","3"]]), "direction")
+        .appendField("が")
+        .appendField(new Blockly.FieldDropdown([["道","this.movableLayer"], ["障害物","this.obstacleLayer"]]), "thing");
+    this.setOutput(true, null);
+    this.setColour(60);
+    this.setTooltip("");
+    this.setHelpUrl("");
+  }
+};
+Blockly.JavaScript['check'] = function(block) {
+  var dropdown_direction = block.getFieldValue('direction');
+  var dropdown_thing = block.getFieldValue('thing');
+  // TODO: Assemble JavaScript into code variable.
+  var code = `this.checkIf(this.player,${dropdown_direction},${dropdown_thing})\n`;
+  return  [code, Blockly.JavaScript.ORDER_NONE];
+};
--- src/Scenes/SceneGame.js
+++ src/Scenes/SceneGame.js
@@ -189,7 +189,7 @@ class SceneGame extends Phaser.Scene {
         const dy = [0, 0, -1, 1];
         const nextGX = player.gridX + dx[dir];
         const nextGY = player.gridY + dy[dir];
-        if (this.obstacleLayer&&this.obstacleLayer.hasTileAt(nextGX,nextGY)> 0) {
+        if (this.obstacleLayer&&this.obstacleLayer.hasTileAt(nextGX,nextGY)) {
           //向いている方向に障害物がある場合、それを取り除く
           this.obstacleLayer.removeTileAt(nextGX,nextGY,false);
           console.log("remove!");
@@ -199,8 +199,16 @@ class SceneGame extends Phaser.Scene {
     getDirection(player){//向いている方向を検知する(どうやってやるんや)
         //todo:この中身を実装する
         //0:right,1;left,2:up,3,downを返すように
-        let num=parseInt(this.player.frame.name);
+        let num=parseInt(player.frame.name);
+        console.log("direction:"+[3,1,0,2][Math.floor(num/3)]);
         return [3,1,0,2][Math.floor(num/3)];
+    }
+    changeDirection(player,dir){
+        let num=parseInt(player.frame.name);
+        console.log("changeDirection");
+        if(dir==0)player.setFrame( [3,9,0,6][Math.floor(num/3)]+1 );//右
+        else player.setFrame( [6,0,9,3][Math.floor(num/3)]+1 );//左
+        console.log(player.frame.name)
     }
     clearGame(){
         console.log("goal");
@@ -279,6 +287,23 @@ class SceneGame extends Phaser.Scene {
             this.player=this.player.setTexture("player2");
         }else{
             this.player.setTexture("player");
+        }
+    }
+    checkIf(player,direction,layer){
+        var player_direction=this.getDirection(player);////0:right,1;left,2:up,3,downを返すように
+        var dir=0;
+        if(direction==0)dir=[0,1,2,3][player_direction];//前
+        if(direction==1)dir=[3,2,0,1][player_direction];//右
+        if(direction==2)dir=[2,3,1,0][player_direction];//左
+        if(direction==3)dir=[1,0,3,2][player_direction];//後ろ
+        const dx = [1, -1, 0, 0];
+        const dy = [0, 0, -1, 1];
+        const nextGX = player.gridX + dx[dir];
+        const nextGY = player.gridY + dy[dir];
+        if (layer.hasTileAt(nextGX,nextGY)) {
+          return true;
+        }else{
+            return false;
         }
     }
 } 
